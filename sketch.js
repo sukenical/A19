f87b3fb6d8aef5d43463b244c057f2b12affbcf5
sukenical <sukenical@mx.lakeforest.edu>
2017-02-15 23:22:16 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,301 @@
+function setup() {
+createCanvas(600, 600);
+}
+function draw() {
+background(255);
+    
+    /*
+    .st0{fill:#5A261E;}
+	.st1{fill:#120D10;}
+	.st2{fill:#B2B1AE;}
+	.st3{fill:#8D8E80;}
+	.st4{fill:#4B2220;}
+	.st5{fill:#5F241C;}
+	.st6{fill:#B0AEA9;}
+	.st7{fill:#141217;}
+	.st8{fill:#C78271;}
+	.st9{fill:#77291E;}
+	.st10{fill:#60241B;}
+	.st11{fill:#B0A99D;}
+	.st12{fill:#B78760;}
+	.st13{fill:#BA2D21;}
+	.st14{fill:#131215;}
+	.st15{fill:#E1DAC7;}
+	.st16{fill:#E3DBCA;}
+	.st17{fill:#782A1E;}
+	.st18{fill:#15141A;}
+	.st19{fill:#672522;}
+	.st20{fill:#B7B6AF;}
+    .st26{fill:#B7B6AF;}
+    */
+ 
+    /*
+    <path class="st0" d="M321.1,274.7c0.6,7.6,0.9,15.2,0.9,23c0,31.4-5.2,61.6-14.8,89.5l-38.6-37.8L321.1,274.7z"/>
+			<path class="st0" d="M599.5,1v95.7L430.7,507.9L307.3,387.1c9.6-27.9,14.8-58,14.8-89.5c0-7.7-0.3-15.4-0.9-23L513.9,0.5L599.5,1
+				z"/>
+    */
+    fill('#120D10');  
+    beginShape(); 			
+    vertex(586, 600);
+    vertex(530, 600);
+    vertex(430, 507);
+    endShape(CLOSE);		
+    
+    /*
+    <path class="st2" d="M268.4,139.7c-19.9-27.6-47.7-56.8-84.9-77c59.6,30.7,105.7,86.3,126.5,154l-0.2,0.5c0,0-0.1-0.2-0.2-0.5
+				c-0.1-0.3-0.2-0.6-0.3-1c0,0,0,0,0,0c-0.1-0.3-0.2-0.6-0.4-1c-0.1-0.2-0.1-0.4-0.2-0.6c-3.5-9.5-15.4-39.4-38.6-71.9
+				C269.5,141.3,269,140.5,268.4,139.7z"/>
+			<path class="st2" d="M309.3,215.6c0.1,0.4,0.3,0.8,0.3,1C309.5,216.4,309.4,216.1,309.3,215.6z"/>
+			<path class="st2" d="M308.9,214.6c0.1,0.4,0.2,0.7,0.4,1C309.1,215.3,309,215,308.9,214.6z"/>
+			<path class="st2" d="M270.1,142.1c23.1,32.5,35.1,62.4,38.6,71.9C305.2,204.6,293.1,174.7,270.1,142.1z"/>
+			<path class="st2" d="M99.3,37.8C70.7,35.4,38.7,38.3,3,48.4c23.2-7.7,47.8-11.8,73.3-11.8C84.1,36.6,91.8,37,99.3,37.8z"/>
+			<path class="st2" d="M3,48.4c-1.1,0.3-2.3,0.7-3.5,1L-1,0l400.9,0.5L310,216.6c-20.8-67.7-66.9-123.2-126.5-154
+				c-24-13-51.9-22.3-84.1-24.9c-7.6-0.8-15.3-1.1-23-1.1C50.8,36.6,26.1,40.8,3,48.4z"/>
+			<path class="st3" d="M122.4,554.1c-0.2,0-0.3,0.1-0.5,0.1c-14.8,3-30,4.5-45.6,4.5c-7.9,0-15.7-0.4-23.4-1.2
+				C25.7,554,3.5,547,0,545.9c-0.3-0.1-0.5-0.2-0.5-0.2V434.5L122.4,554.1z"/>
+    */            
+                	
+    fill('#4B2220');
+    beginShape(); 
+    vertex(169, 600);
+    vertex(154, 600);
+    vertex(159, 590);
+    endShape(CLOSE);	
+			
+    /*
+        <path class="st5" d="M122.4,554.1l47.2,45.9h-0.5l-10-9.7l-4.6,9.8L92,600.5L122.4,554.1C122.3,554.1,122.4,554.1,122.4,554.1z"
+				/>
+			<path class="st5" d="M122.4,554.1L122.4,554.1C122.4,554.1,122.3,554.1,122.4,554.1L122.4,554.1z"/>
+    */
+		
+    fill('#B0AEA9');
+    beginShape();             
+    vertex(92, 600);
+    vertex(0, 600);
+    vertex(0, 550);
+    endShape(CLOSE);	
+			
+    /*    
+    <path class="st7" d="M122.4,554.1L122.4,554.1c-0.2,0.1-0.3,0.1-0.5,0.1C122.1,554.2,122.2,554.1,122.4,554.1z"/>
+			<path class="st7" d="M122.3,554.1L91.7,600l-92.2-49.2v-5.1c0,0,0.2,0,0.5,0.2c16.9,5.9,34.6,9.9,52.9,11.7
+				c22.1,2.9,47.6,3.4,69-3.3C122.1,554.2,122.2,554.2,122.3,554.1z"/>
+    */            
+        
+	
+    fill('#141217');
+    beginShape();                 
+    vertex(122, 554);
+    vertex(92, 600);
+    vertex(0, 550);
+    vertex(91, 600); 			
+    endShape(CLOSE);
+
+        
+    /*   
+    <path class="st7" d="M52.9,557.5c-18.3-1.8-36-5.8-52.9-11.7C3.5,547,25.7,554,52.9,557.5z"/>
+			<path class="st8" d="M193.4,527.2L169.6,600l-47.2-45.9C147.6,549,171.5,539.8,193.4,527.2z"/>
+			<path class="st8" d="M236.5,395.1l-43.1,132.2c-21.9,12.6-45.7,21.8-71,26.9l0,0L236.5,395.1z"/>
+			<path class="st9" d="M236.8,394.7l53.6,31.3c-22.7,42.6-56.4,77.8-96.9,101.2L236.8,394.7z"/>
+			<path class="st9" d="M430.7,507.9L367,600.5L169.6,600l23.8-72.8h0c40.6-23.4,74.2-58.6,96.9-101.2L430.7,507.9z"/>
+    */        
+            
+	
+    fill('#60241B');
+    beginShape();             
+    vertex(430, 507);
+    vertex(395, 600);
+    vertex(367, 600); 			
+	endShape(CLOSE);
+    
+    
+    
+    fill('#77291E');
+    beginShape();     
+    vertex(430, 507);
+    vertex(367, 600);
+    vertex (367, 600);
+	endShape(CLOSE);
+    
+    
+    fill('#60241B');
+    beginShape();     
+    vertex(530, 600);
+    vertex(395, 600);
+    vertex(430,507); 			
+    endShape(CLOSE);
+    
+    
+    
+    fill('#B0A99D');
+    beginShape();     
+    vertex(599, 270);
+    vertex(599, 600);
+    vertex (586, 600);
+    vertex (430, 507); 			
+	endShape(CLOSE);
+    
+    
+    fill('#B78760');
+    beginShape();     
+    vertex(236, 395);
+    vertex (236, 394);
+    vertex (236, 394); 		
+    endShape(CLOSE);
+    
+    
+    fill('#C78271');
+    beginShape();     
+    vertex(236, 394);
+    vertex(236, 395);
+    vertex(122, 554);
+    vertex(236, 394); 			
+    endShape(CLOSE);
+    
+    fill('#B78760');
+    beginShape();     
+    vertex(236, 394);
+    vertex(122, 554);
+    vertex(0, 434);
+    vertex(0,250); 			
+    endShape(CLOSE);
+        
+        
+     /*   
+    <path class="st0" d="M430.7,507.9L290.4,426c6.6-12.3,12.2-25.3,16.9-38.8L430.7,507.9z"/>
+			<path class="st0" d="M268.6,349.4l38.6,37.8c-4.6,13.5-10.3,26.5-16.9,38.8l-53.6-31.3L268.6,349.4z"/>
+            
+            */
+fill('#BA2D21');
+beginShape();            
+vertex(599, 96);
+vertex(599, 270);
+vertex(430, 507); 
+endShape(CLOSE);
+
+
+/*
+			<path class="st14" d="M513.9,0.5L321.1,274.7c-1.6-19.9-5.4-39.1-10.9-57.4L399.9,0.5H513.9z"/>
+			<path class="st14" d="M321.1,274.7l-52.5,74.7l-31.8,45.3l21.7-52.5l51.7-124.9C315.8,235.6,319.5,254.8,321.1,274.7z"/>
+            */
+fill('#E1DAC7');
+beginShape();           
+vertex(0, 92);
+vertex(258, 342);
+vertex(258, 342); 
+vertex(0, 92);
+endShape(CLOSE);
+            
+
+fill('#E3DBCA');
+beginShape(); 
+vertex(236, 394);
+vertex(236, 394);
+vertex(236, 394);
+vertex(236, 394);
+vertex(0, 250);
+vertex(0,92);
+vertex(258, 342);
+vertex(258, 342);
+vertex(258, 342);
+endShape(CLOSE);
+	
+    
+fill('#C78271');
+beginShape();                
+vertex(236, 394);
+vertex(236, 394);
+vertex(236, 394); 			
+endShape(CLOSE);
+    
+    
+    
+    /*        
+    <path class="st15" d="M0,92.2v0.5l-0.5-0.5V49.6c0.9-0.3,1.8-0.6,2.7-0.9c0,0,0,0,0,0c139.2-38.1,222.1,30.3,266.1,91
+				c0.6,0.8,1.1,1.6,1.7,2.4c23,32.6,35.1,62.4,38.6,71.9c0.1,0.2,0.1,0.4,0.2,0.6c0.1,0.4,0.2,0.7,0.4,1c0,0,0,0,0,0
+				c0.1,0.4,0.3,0.8,0.3,1c0.1,0.3,0.2,0.5,0.2,0.5L258.3,342L0,92.2z"/>
+    */
+fill('#5A261E');
+beginShape();   
+vertex(430, 508);
+vertex(236, 394);
+vertex(513, 0);
+vertex(599, 1);
+vertex(599, 97);
+endShape(CLOSE);	
+
+fill('#782A1E');
+beginShape(); 
+vertex(169, 601);
+vertex(236, 395);
+vertex(430, 508);
+vertex(367, 601);
+endShape(CLOSE);
+
+fill('#15141A');
+beginShape();
+vertex(236, 395);
+vertex(513, 0);
+vertex(399, 0);
+endShape(CLOSE);
+    
+fill('#672522');
+beginShape();    
+vertex(122, 554);
+vertex(159, 590);
+vertex(154, 601);
+vertex(92, 601);
+endShape(CLOSE);  
+ 
+fill('#C78271');
+beginShape();  
+vertex(236, 395);
+vertex(169, 601);
+vertex(122, 554);
+endShape(CLOSE);
+    
+fill('#8D8E80');
+beginShape();    
+vertex(0, 435);
+vertex(0, 551);
+vertex(91, 601);
+vertex(121, 554);    
+endShape(CLOSE);
+    
+fill('#B7B6AF');
+beginShape();   
+vertex(0, 0);
+vertex(399, 0);
+vertex(258, 342);
+vertex(0, 92);    
+endShape(CLOSE);
+    
+fill('#B7B6AF');
+beginShape();     
+vertex(0, 53);
+vertex(0, 544);
+vertex(32, 554);
+vertex(74, 559);
+vertex(120, 556);
+vertex(164, 544);
+vertex(200, 526);
+vertex(234, 501);
+vertex(264, 470); 
+vertex(289, 433);
+vertex(306, 394);
+vertex(317, 354);
+vertex(322, 311);
+vertex(322, 269);
+vertex(314, 231);
+vertex(302, 195);
+vertex(285, 161);
+vertex(261, 128);
+vertex(234, 101);
+vertex(204, 79);
+vertex(170, 58);
+vertex(129, 43);
+vertex(90, 40);
+vertex(48, 40);
+vertex(20, 45);    
+endShape(CLOSE);
+}
+
